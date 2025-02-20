e40827b324245763bfead10f59227e058fe23246
RAHUL C H <46349786+sudotturi@users.noreply.github.com>
2025-02-20 17:27:26 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,30 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: process.env.CODESPACES
-          ? [
-              {
-                key: 'X-Forwarded-Host',
-                value: `${process.env.CODESPACE_NAME}-3000.app.github.dev`,
-              },
-              {
-                key: 'Access-Control-Allow-Origin',
-                value: `https://${process.env.CODESPACE_NAME}-3000.app.github.dev`,
-              },
-            ]
-          : [],
-      },
-    ]
-  }, 
-  experimental: {
-		serverActions: {
-			allowedOrigins: ["*.github.dev"]
-			},
-	}
+
 };
 
 export default nextConfig;
